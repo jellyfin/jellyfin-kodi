1a472a43cd9ef07862b1b0aab799c8f7cb56322a
angelblue05 <angelblue05@users.noreply.github.com>
2015-03-22 03:07:06 -0500

    Fixed post capabilities
    Client was not showing as a player in the web client.

--- resources/lib/DownloadUtils.py
+++ resources/lib/DownloadUtils.py
@@ -111,7 +111,7 @@ class DownloadUtils():
         supportedCommands = "Play,Playstate,DisplayContent,GoHome,SendString,GoToSettings,DisplayMessage,PlayNext"
         
         url = "http://" + mb3Host + ":" + mb3Port + "/mediabrowser/Sessions/Capabilities?Id=" + sessionId + "&PlayableMediaTypes=" + playableMediaTypes + "&SupportedCommands=" + supportedCommands
-        
+  + "&SupportsMediaControl=True"
         postData = {}
         #postData["Id"] = sessionId;
         #postData["PlayableMediaTypes"] = "Video";
