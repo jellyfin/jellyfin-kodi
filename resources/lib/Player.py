7454b022f702bbc6a8aa281e2ce5b3d41411198b
angelblue05 <tamara.angel05@gmail.com>
2015-05-16 03:52:30 -0500

    Seek playback
    Reflect more accurately in the dashboard

--- resources/lib/Player.py
+++ resources/lib/Player.py
@@ -208,6 +208,13 @@ class Player( xbmc.Player ):
     
     def onPlayBackSeek( self, time, seekOffset ):
         self.logMsg("PLAYBACK_SEEK",2)
+        # Make position when seeking a bit more accurate
+        try:
+            playTime = xbmc.Player().getTime()
+            currentFile = xbmc.Player().getPlayingFile()
+            if(self.played_information.get(currentFile) != None):
+                self.played_information[currentFile]["currentPosition"] = playTime
+        except: pass
         self.reportPlayback()
         
     def onPlayBackStarted( self ):
