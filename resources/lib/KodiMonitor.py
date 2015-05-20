b556707ce65c6c23d556f72d7e67774e72444c9c
xnappo <cnappi@gmail.com>
2015-05-19 21:03:53 -0500

    Fix for multiple OnWake messages

--- resources/lib/KodiMonitor.py
+++ resources/lib/KodiMonitor.py
@@ -55,9 +55,11 @@ class Kodi_Monitor(xbmc.Monitor):
                     
         if method == "System.OnWake":
             xbmc.sleep(10000) #Allow network to wake up
-            utils.logMsg("Doing_Db_Sync Post Resume: syncDatabase (Started)",1)
-            libSync = LibrarySync().FullLibrarySync()
-            utils.logMsg("Doing_Db_Sync Post Resume: syncDatabase (Finished) " + str(libSync),1)
+            if WINDOW.getProperty("SyncDatabaseRunning") != "true":
+                utils.logMsg("Doing_Db_Sync Post Resume: syncDatabase (Started)",0)
+                libSync = LibrarySync().FullLibrarySync()
+                utils.logMsg("Doing_Db_Sync Post Resume: syncDatabase (Finished) " + str(libSync),0)
+
 
         if method == "VideoLibrary.OnRemove":
             xbmc.log('Intercepted remove from sender: ' + sender + ' method: ' + method + ' data: ' + data)
