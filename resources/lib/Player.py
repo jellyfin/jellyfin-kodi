9197e274d2bea165c1aa7e37f5b71729d58c3de9
angelblue05 <tamara.angel05@gmail.com>
2015-05-23 17:51:41 -0500

    First attempt - fix for slow playback start
    Removing the json call, might cause lock up on low end devices and/or
    slow playback start.

--- resources/lib/Player.py
+++ resources/lib/Player.py
@@ -252,12 +252,12 @@ class Player( xbmc.Player ):
             itemType = WINDOW.getProperty(currentFile + "type")
             seekTime = WINDOW.getProperty(currentFile + "seektime")
             
-            # Get playback volume
+            '''# Get playback volume
             volume_query = '{"jsonrpc": "2.0", "method": "Application.GetProperties", "params": {"properties": ["volume","muted"]}, "id": 1}'
             result = xbmc.executeJSONRPC(volume_query)
             result = json.loads(result)
             volume = result.get(u'result').get(u'volume')
-            muted = result.get(u'result').get(u'muted')
+            muted = result.get(u'result').get(u'muted')'''
             
             if seekTime:
                 PlaybackUtils().seekToPosition(int(seekTime))
@@ -271,9 +271,7 @@ class Player( xbmc.Player ):
                 'ItemId': item_id,
                 'MediaSourceId': item_id,
                 'PlayMethod': playMethod,
-                'VolumeLevel': volume,
-                'PositionTicks': int(seekTime),
-                'IsMuted': muted
+                'PositionTicks': int(seekTime)
             }
 
             if audioindex:
