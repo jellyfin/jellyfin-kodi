7f2610642b1dd26c849869deb410bada9523551f
angelblue05 <tamara.angel05@gmail.com>
2015-06-16 20:24:54 -0500

    Quick follow up
    Prevent from picking up the temp journal file Kodi creates.

--- resources/lib/Utils.py
+++ resources/lib/Utils.py
@@ -66,7 +66,7 @@ def getKodiVideoDBPath():
     dirs, files = xbmcvfs.listdir("special://database")
     dbVersion = ""
     
-    for database in files:
+    for database in files and "journal" not in database:
         if "MyVideos" in database:
             dbVersion = database
 
@@ -80,7 +80,7 @@ def getKodiMusicDBPath():
     dirs, files = xbmcvfs.listdir("special://database")
     dbVersion = ""
 
-    for database in files:
+    for database in files and "journal" not in database:
         if "MyMusic" in database:
             dbVersion = database
 
