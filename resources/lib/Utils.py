6e56d259d430e80f80fd595ce27f16355eab7312
im85288 <ianmalcolmmclaughlin@gmail.com>
2015-06-20 18:05:54 +0100

    merged normalize string changes from tvtunes addon

--- resources/lib/Utils.py
+++ resources/lib/Utils.py
@@ -167,6 +167,11 @@ def normalize_string(text):
         text = text.replace(":", "")
         text = text.replace("/", "-")
         text = text.replace("\\", "-")
+        text = text.replace("<", "")
+        text = text.replace(">", "")
+        text = text.replace("*", "")
+        text = text.replace("?", "")
+        text = text.replace('|', "")
         text = text.strip()
         # Remove dots from the last character as windows can not have directories
         # with dots at the end
