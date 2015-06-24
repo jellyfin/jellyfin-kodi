feb6c73fc33e71c177324fa8e935ade4dc393efa
Marcel van der Veldt <m.vanderveldt@outlook.com>
2015-06-24 10:02:34 +0200

    fix texture cache stuff

--- resources/lib/WriteKodiVideoDB.py
+++ resources/lib/WriteKodiVideoDB.py
@@ -792,10 +792,8 @@ class WriteKodiVideoDB():
                     cursor.execute("UPDATE art set url = ? WHERE media_id = ? AND media_type = ? AND type = ?", (imageUrl, kodiId, mediaType, imageType))
                     
             #cache fanart and poster in Kodi texture cache
-            if "fanart" in imageType:
+            if "fanart" in imageType or "poster" in imageType:
                 utils.logMsg("ArtworkSync", "Adding or Updating Fanart: %s" % imageUrl)
-                self.textureCache.refreshFanart(imageUrl)
-            elif "poster" in imageType:
                 self.textureCache.CacheTexture(imageUrl)
         
     def setKodiResumePoint(self, fileid, resume_seconds, total_seconds, cursor):
