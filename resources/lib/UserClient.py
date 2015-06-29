385c3cae7e593ce6c17268f906502f5bdd0ffd74
angelblue05 <tamara.angel05@gmail.com>
2015-06-29 09:09:29 -0500

    Revert "Revert "Support alternate address""
    This reverts commit ef926f94e2a81f8d258884626264a18c275ee7d1.

--- resources/lib/UserClient.py
+++ resources/lib/UserClient.py
@@ -96,6 +96,10 @@ class UserClient(threading.Thread):
         HTTPS = addon.getSetting('https')
         host = addon.getSetting('ipaddress')
         port = addon.getSetting('port')
+        # Alternate host
+        if addon.getSetting('altip') == "true":
+            host = addon.getSetting('secondipaddress')
+            
         server = host + ":" + port
         
         if host == "":
