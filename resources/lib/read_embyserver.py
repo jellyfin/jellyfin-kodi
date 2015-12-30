08354b693ebaa99291c06a1f1f269ae6b22ce113
angelblue05 <tamara.angel05@gmail.com>
2015-12-29 19:32:41 -0600

    Fix specials ordering

--- resources/lib/read_embyserver.py
+++ resources/lib/read_embyserver.py
@@ -81,7 +81,7 @@ class Read_EmbyServer():
                         "CommunityRating,OfficialRating,CumulativeRunTimeTicks,"
                         "Metascore,AirTime,DateCreated,MediaStreams,People,Overview,"
                         "CriticRating,CriticRatingSummary,Etag,ProductionLocations,"
-                        "Tags,ProviderIds,RemoteTrailers"
+                        "Tags,ProviderIds,RemoteTrailers,SpecialEpisodeNumbers"
                 )
             }
             result = self.doUtils.downloadUrl(url, parameters=params)
@@ -182,7 +182,7 @@ class Read_EmbyServer():
                         "CommunityRating,OfficialRating,CumulativeRunTimeTicks,"
                         "Metascore,AirTime,DateCreated,MediaStreams,People,Overview,"
                         "CriticRating,CriticRatingSummary,Etag,ShortOverview,ProductionLocations,"
-                        "Tags,ProviderIds,ParentId,RemoteTrailers"
+                        "Tags,ProviderIds,ParentId,RemoteTrailers,SpecialEpisodeNumbers"
                     )
                 result = doUtils.downloadUrl(url, parameters=params)
                 items['Items'].extend(result['Items'])
