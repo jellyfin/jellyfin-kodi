db4cb448b0e4fd82662f8b82a800d8da8ea33688
angelblue05 <tamara.angel05@gmail.com>
2016-03-09 20:15:45 -0600

    Adjust delete logic for entrypoint

--- resources/lib/entrypoint.py
+++ resources/lib/entrypoint.py
@@ -118,30 +118,30 @@ def resetDeviceId():
             line1=language(33033))
         xbmc.executebuiltin('RestartApp')
 
-##### Delete Item, like the context menu action
+##### Delete Item
 def deleteItem():
 
     # Serves as a keymap action
-    dbid = xbmc.getInfoLabel('ListItem.DBID')
-    itemtype = xbmc.getInfoLabel('ListItem.DBTYPE')
-
-    if not itemtype:
-
-        if xbmc.getCondVisibility('Container.Content(albums)'):
-            itemtype = "album"
-        elif xbmc.getCondVisibility('Container.Content(artists)'):
-            itemtype = "artist"
-        elif xbmc.getCondVisibility('Container.Content(songs)'):
-            itemtype = "song"
-        elif xbmc.getCondVisibility('Container.Content(pictures)'):
-            itemtype = "picture"
-        else:
-            utils.logMsg("EMBY delete", "Unknown type, unable to proceed.", 1)
-            return
-
     if xbmc.getInfoLabel('ListItem.Property(embyid)'): # If we already have the embyid
         embyid = xbmc.getInfoLabel('ListItem.Property(embyid)')
     else:
+        dbid = xbmc.getInfoLabel('ListItem.DBID')
+        itemtype = xbmc.getInfoLabel('ListItem.DBTYPE')
+
+        if not itemtype:
+
+            if xbmc.getCondVisibility('Container.Content(albums)'):
+                itemtype = "album"
+            elif xbmc.getCondVisibility('Container.Content(artists)'):
+                itemtype = "artist"
+            elif xbmc.getCondVisibility('Container.Content(songs)'):
+                itemtype = "song"
+            elif xbmc.getCondVisibility('Container.Content(pictures)'):
+                itemtype = "picture"
+            else:
+                utils.logMsg("EMBY delete", "Unknown type, unable to proceed.", 1)
+                return
+
         embyconn = utils.kodiSQL('emby')
         embycursor = embyconn.cursor()
         emby_db = embydb.Embydb_Functions(embycursor)
