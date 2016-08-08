51400fd8282441c3fe7f9de0d62588900c0078db
angelblue05 <tamara.angel05@gmail.com>
2016-08-07 19:57:11 -0500

    Add JSONRPC class

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -51,6 +51,36 @@ def language(string_id):
     # Central string retrieval - unicode
     return xbmcaddon.Addon(id='plugin.video.emby').getLocalizedString(string_id)
 
+class JSONRPC(object):
+
+    id_ = 1
+    jsonrpc = "2.0"
+
+    def __init__(self, method, **kwargs):
+        
+        self.method = method
+
+        for arg in kwargs: # id_(int), jsonrpc(str)
+            self.arg = arg
+
+    def _query(self):
+
+        query = {
+            
+            'jsonrpc': self.jsonrpc,
+            'id': self.id_,
+            'method': self.method,
+        }
+        if self.params is not None:
+            query['params'] = self.params
+
+        return json.dumps(query)
+
+    def execute(self, params=None):
+
+        self.params = params
+        return json.loads(xbmc.executeJSONRPC(self._query()))
+
 #################################################################################################
 # Database related methods
 
