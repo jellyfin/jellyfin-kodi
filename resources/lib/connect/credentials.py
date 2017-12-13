bfaed955f52345e7d404103ba77d467c7b921778
angelblue05 <tamara.angel05@gmail.com>
2017-12-13 02:37:40 -0600

    Attempt to fix encoding error

--- resources/lib/connect/credentials.py
+++ resources/lib/connect/credentials.py
@@ -27,7 +27,7 @@ class Credentials(object):
 
     def setPath(self, path):
         # Path to save persistant data.txt
-        self.path = path
+        self.path = path.decode('utf-8')
 
     def _ensure(self):
         
