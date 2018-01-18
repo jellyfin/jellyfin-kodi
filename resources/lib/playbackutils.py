a9e4cebed43b136bc527d90983631a7ac4cb75e0
Sebastian S <sualfred@users.noreply.github.com>
2018-01-19 00:49:02 +0100

    Changed playback method detection (#123)

--- resources/lib/playbackutils.py
+++ resources/lib/playbackutils.py
@@ -88,8 +88,8 @@ class PlaybackUtils(object):
         # Stack: [(url, listitem), (url, ...), ...]
         self.stack[0][1].setPath(self.stack[0][0])
         try:
-            if (not xbmc.getCondVisibility('Window.IsVisible(MyVideoNav.xml)') and
-                not xbmc.getCondVisibility('Window.IsVisible(VideoFullScreen.xml)')):
+            if (not xbmc.getCondVisibility('Window.IsMedia') and
+                not xbmc.getCondVisibility('Integer.IsGreater(Playlist.Length(video),1)')):
                 # widgets do not fill artwork correctly
                 log.info("Detected widget.")
                 raise IndexError
