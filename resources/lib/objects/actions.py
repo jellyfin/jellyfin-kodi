4ecd3fa2f62a2adc791de9084afc57b0f0289d34
angelblue05 <angelblue.dev@gmail.com>
2018-10-06 18:44:15 -0500

    Add force transcode settings

--- resources/lib/objects/actions.py
+++ resources/lib/objects/actions.py
@@ -52,13 +52,14 @@ class Actions(object):
         listitem = xbmcgui.ListItem()
         LOG.info("[ play/%s ] %s", item['Id'], item['Name'])
 
-        playlist = self.get_playlist(item)
+        transcode = transcode or settings('playFromTranscode.bool')
+        kodi_playlist = self.get_playlist(item)
         play = playutils.PlayUtils(item, transcode, self.server_id, self.server)
         source = play.select_source(play.get_sources())
         play.set_external_subs(source, listitem)
 
         self.set_playlist(item, listitem, db_id, transcode)
-        index = max(playlist.getposition(), 0) + 1 # Can return -1
+        index = max(kodi_playlist.getposition(), 0) + 1 # Can return -1
         force_play = False
 
         self.stack[0][1].setPath(self.stack[0][0])
@@ -75,12 +76,12 @@ class Actions(object):
 
         for stack in self.stack:
 
-            playlist.add(url=stack[0], listitem=stack[1], index=index)
+            kodi_playlist.add(url=stack[0], listitem=stack[1], index=index)
             index += 1
 
         if force_play:
             if len(sys.argv) > 1: xbmcplugin.setResolvedUrl(int(sys.argv[1]), False, self.stack[0][1])
-            xbmc.Player().play(playlist, windowed=False)
+            xbmc.Player().play(kodi_playlist, windowed=False)
 
     def set_playlist(self, item, listitem, db_id=None, transcode=False):
 
