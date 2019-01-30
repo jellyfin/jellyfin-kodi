158a736360b6419fa20f0b48e2fb2cf16e83a992
angelblue05 <angelblue.dev@gmail.com>
2019-01-30 06:43:14 -0600

    Update webservice with cherrypy
    Fix playback issues that was causing Kodi to hang up

---
+++ libraries/cheroot/__init__.py
@@ -0,0 +1,6 @@
+"""High-performance, pure-Python HTTP server used by CherryPy."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+__version__ = '6.4.0'
---
+++ libraries/zc/__init__.py
@@ -0,0 +1,1 @@
+__namespace__ = 'zc'
