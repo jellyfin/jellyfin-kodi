7149cd1917f172bc54178c4cef889f203b147cf1
angelblue05 <angelblue.dev@gmail.com>
2019-02-01 09:13:12 -0600

    Fix import error
    Fix context menu

--- context.py
+++ context.py
@@ -13,7 +13,7 @@ import xbmcaddon
 
 __addon__ = xbmcaddon.Addon(id='plugin.video.emby')
 __base__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('path'), 'resources', 'lib')).decode('utf-8')
-__libraries__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('path'), 'librarie')).decode('utf-8')
+__libraries__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('path'), 'libraries')).decode('utf-8')
 __pcache__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('profile'), 'emby')).decode('utf-8')
 __cache__ = xbmc.translatePath('special://temp/emby').decode('utf-8')
 
