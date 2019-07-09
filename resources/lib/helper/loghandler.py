c559f2fff6e17419c977e637f53267740e9b99ff
Odd Stråbø <oddstr13@openshell.no>
2019-07-10 00:18:40 +0200

    Strip pluginpath from logged stacktraces

--- resources/lib/helper/loghandler.py
+++ resources/lib/helper/loghandler.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import os
 import logging
+import traceback
 
 import xbmc
 import xbmcaddon
@@ -118,6 +119,22 @@ class MyFormatter(logging.Formatter):
         self._fmt = format_orig
 
         return result
+
+    def formatException(self, exc_info):
+        _pluginpath_real = os.path.realpath(__pluginpath__)
+        res = []
+
+        for o in traceback.format_exception(*exc_info):
+            if o.startswith('  File "'):
+                # If this split can't handle your file names, you should seriously consider renaming your files.
+                fn = o.split('  File "', 2)[1].split('", line ', 1)[0]
+                rfn = os.path.realpath(fn)
+                if rfn.startswith(_pluginpath_real):
+                    o = o.replace(fn, os.path.relpath(rfn, _pluginpath_real))
+
+            res.append(o)
+
+        return ''.join(res)
 
     def _gen_rel_path(self, record):
         if record.pathname:
