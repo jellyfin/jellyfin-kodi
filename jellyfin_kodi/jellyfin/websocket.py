9ddab3fc9f99f7a2313ba01ea4d490b8733a0f58
swedge <scott.wedge@gmail.com>
2020-04-18 21:39:26 -0400

    Fixes #278

--- jellyfin_kodi/jellyfin/websocket.py
+++ jellyfin_kodi/jellyfin/websocket.py
@@ -373,7 +373,7 @@ class WebSocket(object):
       The WebSocket protocol draft-hixie-thewebsocketprotocol-76
       http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-76
 
-    We can connect to the websocket server and send/recieve data.
+    We can connect to the websocket server and send/receive data.
     The following example is a echo client.
 
     >>> import websocket
@@ -635,7 +635,7 @@ class WebSocket(object):
 
     def recv_data(self):
         """
-        Recieve data with operation code.
+        Receive data with operation code.
 
         return  value: tuple of operation code and string(byte array) value.
         """
@@ -665,7 +665,7 @@ class WebSocket(object):
 
     def recv_frame(self):
         """
-        recieve data as frame from server.
+        receive data as frame from server.
 
         return value: ABNF frame object.
         """
@@ -821,7 +821,7 @@ class WebSocketApp(object):
         header: custom header for websocket handshake.
         on_open: callable object which is called at opening websocket.
           this function has one argument. The arugment is this class object.
-        on_message: callbale object which is called when recieved data.
+        on_message: callbale object which is called when received data.
          on_message has 2 arguments.
          The 1st arugment is this class object.
          The passing 2nd arugment is utf-8 string which we get from the server.
