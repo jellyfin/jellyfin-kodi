db51abd786516b4106c609140ace181a1e653ec0
Odd Stråbø <oddstr13@openshell.no>
2020-05-31 01:07:16 +0200

    Add Kodi 17 backwards compat to #308

--- jellyfin_kodi/dialogs/loginmanual.py
+++ jellyfin_kodi/dialogs/loginmanual.py
@@ -8,6 +8,7 @@ from kodi_six import xbmcgui
 
 from helper import translate
 from helper import LazyLogger
+from helper import kodi_version
 
 ##################################################################################################
 
@@ -97,13 +98,20 @@ class LoginManual(xbmcgui.WindowXMLDialog):
 
     def _add_editcontrol(self, x, y, height, width, password=False):
 
-        control = xbmcgui.ControlEdit(0, 0, 0, 0,
-                                      label="User",
-                                      font="font13",
-                                      textColor="FF00A4DC",
-                                      disabledColor="FF888888",
-                                      focusTexture="-",
-                                      noFocusTexture="-")
+        kwargs = dict(
+            label="User",
+            font="font13",
+            textColor="FF00A4DC",
+            disabledColor="FF888888",
+            focusTexture="-",
+            noFocusTexture="-"
+        )
+
+        # TODO: Kodi 17 compat removal cleanup
+        if kodi_version() < 18:
+            kwargs['isPassword'] = password
+
+        control = xbmcgui.ControlEdit(0, 0, 0, 0, **kwargs)
 
         control.setPosition(x, y)
         control.setHeight(height)
@@ -112,7 +120,8 @@ class LoginManual(xbmcgui.WindowXMLDialog):
         self.addControl(control)
 
         # setType has no effect before the control is added to a window
-        if password:
+        # TODO: Kodi 17 compat removal cleanup
+        if password and not kodi_version() < 18:
             control.setType(xbmcgui.INPUT_TYPE_PASSWORD, "Please enter password")
 
         return control
