704c8829f0645a6541322cbe26cabda2f468e3a0
Odd Stråbø <oddstr13@openshell.no>
2024-05-18 01:50:50 +0200

    Mark helper.exceptions.HTTPException as deprecated

--- jellyfin_kodi/helper/exceptions.py
+++ jellyfin_kodi/helper/exceptions.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+import warnings
+
 #################################################################################################
 
 
 class HTTPException(Exception):
     # Jellyfin HTTP exception
     def __init__(self, status, message):
+        warnings.warn(f'{self.__class__.__name__} will be deprecated.', DeprecationWarning, stacklevel=2)
         self.status = status
         self.message = message
 
